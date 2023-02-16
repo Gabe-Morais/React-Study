c034ac9e09f389577ffb92d30172cd83eb2c779a
Gabe-Morais <gabe-morais@outlook.com>
2023-02-16 13:17:45 -0300

    Concluindo Hello World

---
+++ 1-intro/hello-world/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() 
+{
+  return (
+    <div className="App">
+      <h1>Hello World React!</h1>
+      <h3>CSS Color Test</h3>
+    </div>
+  )
+}
+
+export default App;
