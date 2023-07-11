bf315a6be0e034b9ca9ff85325e06d3b2e05954e
Gabriel Morais <gabe-morais@outlook.com>
2023-07-11 14:23:06 -0300

    Concluindo a seção de formulários

--- 5-Forms-no-React/form/src/App.js
+++ 5-Forms-no-React/form/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import MyForm from './components/MyForm';
 
@@ -6,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h2>Forms</h2>
-      <MyForm/>
+      <MyForm user={{name:"teste",  email:"teste@teste.com", bio:"Descrição de teste", role: "Admin"}}/>
     </div>
   );
 }
--- 5-Forms-no-React/form/src/components/MyForm.js
+++ 5-Forms-no-React/form/src/components/MyForm.js
@@ -1,22 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./MyForm.css";
 
-const MyForm = () => {
+const MyForm = ({user}) => {
+    //3 - Gerenciament de Dados
+    const [name, setName] = useState(user ? user.name : "");
+    const [email, setEmail] = useState(user ? user.email : "");
+    const [bio, setBio] = useState("");
+    const [role, setRole] = useState("");
+
+    const handleName = (e) => {
+        setName(e.target.value);
+    };
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        console.log("enviando o formulário") 
+        console.log(name, email, bio, role);
+
+         // limpar o formulario após o envio
+        setName("");
+        setEmail("");
+        setBio("");
+    }
+
+   
+
+
     return (
         <div>
+            {/* Envio de form */}
             {/* 1 - criação de form */}
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div>
                 <label htmlFor="name">Nome: </label>
-                <input type="text" name="name" placeholder='Digite o seu nome'/>
+                <input type="text" name="name" value={name} placeholder='Digite o seu nome' onChange={handleName}/>
                 </div>
                 {/* 2 - Label envolvendo o input */}
                 <label>
                     <span>E-mail</span>
-                    <input type="email" name="email" placeholder='Digite o seu email'/>
+                    <input type="email" name="email" value={email} placeholder='Digite o seu email' onChange={(e) => setEmail(e.target.value)}/>
                 </label>
-           
-            <input type="submite" value="enviar"></input> 
+
+                {/* Text Area */}
+                <span>Bio:</span>
+                <textarea name="bio" placeholder='Descrição do usuário' value={bio} onChange={(e) => setBio(e.target.value)}></textarea>
+           {/* Select */}
+           <label>
+                <span>Função do Sistema</span>
+                <select name="role" value={role} onChange={(e) => setRole(e.target.value)}>
+                    <option value="user">Usuário</option>
+                    <option value="editor">Editor</option>
+                    <option value="admin">Admin</option>
+                </select>
+           </label> 
+            <input type="submit" value="enviar"/>
             </form>
         </div>
     );
