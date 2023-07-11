a77c53001803827bdeb2208c8e098ffb4b5c2829
Gabriel Morais <gabe-morais@outlook.com>
2023-07-11 16:26:06 -0300

    Iniciando projeto Secret Word

---
+++ 6-Secret-Word/secretword/src/App.js
@@ -0,0 +1,18 @@
+//css
+import "./App.css";
+
+//react
+
+
+//Components
+import StartScreen from "./components/StartScreen";
+
+function App() {
+  return (
+    <div className="App">
+      <StartScreen />
+    </div>
+  );
+}
+
+export default App;
