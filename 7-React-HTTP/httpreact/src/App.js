4e5780376d15943c81e3e267e334770ef261a750
Gabriel Morais <gabe-morais@outlook.com>
2023-07-26 13:47:07 -0300

    Resgatando dados da api

---
+++ 7-React-HTTP/httpreact/src/App.js
@@ -0,0 +1,39 @@
+import "./App.css";
+
+import { useState, useEffect } from "react";
+
+const url = "http://localhost:3000/products";
+
+function App() {
+  const [products, setProducts] = useState([]);
+
+  //1 -resgatando dados
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Lista de Produtos</h1>
+      <ul>
+        <li>
+          {products.map((product) => (
+            <li key={product.id}>
+              {product.name} - R$: {product.price}{" "}
+            </li>
+          ))}
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
