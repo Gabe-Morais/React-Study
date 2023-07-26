bec78fa0454cb9072688c65317d996fa5f9a9c77
Gabriel Morais <gabe-morais@outlook.com>
2023-07-26 14:53:39 -0300

    Começando Sessão React Router

---
+++ 8-React-Router/reactrouter/src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Router</h1>
+    </div>
+  );
+}
+
+export default App;
