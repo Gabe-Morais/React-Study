84471ab518f5675f9d991a576bfff518422c7cb7
Gabriel Morais <gabe-morais@outlook.com>
2023-07-26 15:46:55 -0300

    Concluindo Sessão Router

--- 8-React-Router/reactrouter/src/App.js
+++ 8-React-Router/reactrouter/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Navbar from "./components/Navbar";
 import Product from "./pages/Product";
 import Info from "./pages/Info";
+import NotFound from "./pages/NotFound";
+import Search from "./pages/Search";
+import SearchForm from "./components/SearchForm";
 
 function App() {
   return (
@@ -14,11 +17,16 @@ function App() {
       <h1>React Router</h1>
       <Router>
         <Navbar />
+        <SearchForm />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/products/:id" element={<Product />} />
           <Route path="/products/:id/info" element={<Info />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/company" element={<Navigate to="/about" />} />
+          {/*Qualquer Rota não definida ira chegar no "*" */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </div>
---
+++ 8-React-Router/reactrouter/src/pages/Search.js
@@ -0,0 +1,31 @@
+// dependencies
+import { useSearchParams, Link } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+
+const Search = () => {
+  const [searchParams] = useSearchParams();
+
+  const url = `http://localhost:3000/products?${searchParams}`;
+
+  const { data: items, loading, error } = useFetch(url);
+
+  return (
+    <div>
+      <h1>Resultados disponíveis:</h1>
+      {error && <p>{error}</p>}
+      {loading && <p>Carregando...</p>}
+      <ul className="products">
+        {items &&
+          items.map((item) => (
+            <li key={item.id}>
+              <h2>{item.name}</h2>
+              <p>R$: {item.price}</p>
+              <Link to={`products/${item.id}`}>Detalhes</Link>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Search;
