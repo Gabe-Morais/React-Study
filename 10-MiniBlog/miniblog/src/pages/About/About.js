a1e3611e4bdd2c8409616dde9710cec905e1bfbf
Gabriel Morais <gabe-morais@outlook.com>
2023-07-28 10:50:42 -0300

    Estilos Gerais

--- 10-MiniBlog/miniblog/src/pages/About/About.js
+++ 10-MiniBlog/miniblog/src/pages/About/About.js
@@ -1,5 +1,5 @@
 //CSS
-import Styles from "./About.module.css";
+import styles from "./About.module.css";
 
 const About = () => {
   return (
--- 10-MiniBlog/miniblog/src/pages/Home/Home.js
+++ 10-MiniBlog/miniblog/src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 //CSS
-import Styles from "./Home.module.css";
+import styles from "./Home.module.css";
 
 const Home = () => {
   return (
