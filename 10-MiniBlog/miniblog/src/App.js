9aece11d4a3a091b3ceace2368a827d1e98dc6e8
Gabriel Morais <gabe-morais@outlook.com>
2023-08-01 13:31:49 -0300

    Adicionando campos Register page

--- 10-MiniBlog/miniblog/src/App.js
+++ 10-MiniBlog/miniblog/src/App.js
@@ -7,6 +7,8 @@ import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </div>
         <Footer />
