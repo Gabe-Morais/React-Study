c066ea50d092464b23226333759873d9d6815e42
Gabriel Morais <gabe-morais@outlook.com>
2023-08-08 11:39:11 -0300

    Custom Hook

--- 11-React-Hooks/reacthooks/src/pages/Home.js
+++ 11-React-Hooks/reacthooks/src/pages/Home.js
@@ -11,6 +11,7 @@ import HookUseCallback from "../components/HookUseCallback";
 import HookUseMemo from "../components/HookUseMemo";
 import HookUseLayoutEffect from "../components/HookUseLayoutEffect";
 import HookUseImperativeHandle from "../components/HookUseImperativeHandle";
+import HookCustom from "../components/HookCustom";
 
 const Home = () => {
   const { contextValue } = useContext(SomeContext);
@@ -28,6 +29,7 @@ const Home = () => {
       <HookUseMemo />
       <HookUseLayoutEffect />
       <HookUseImperativeHandle />
+      <HookCustom />
     </div>
   );
 };
